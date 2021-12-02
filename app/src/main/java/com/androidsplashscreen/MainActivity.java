e9cc4187c20c93cc6b4a9e27bec37fbe9859d85d
Ahmet Faruk EKEN <ekenahmetfaruk@gmail.com>
2021-12-03 01:17:39 +0300

    Splash screen added animation added

--- app/src/main/java/com/androidsplashscreen/MainActivity.java
+++ app/src/main/java/com/androidsplashscreen/MainActivity.java
@@ -2,13 +2,36 @@ package com.androidsplashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int SPLASH_SCREEN = 5000;
+    Animation alpha;
+    ImageView imageViewApartSoftLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        imageViewApartSoftLogo = findViewById(R.id.imageViewApartSoftLogo);
+        alpha = AnimationUtils.loadAnimation(MainActivity.this, R.anim.animation_alpha);
+        imageViewApartSoftLogo.setAnimation(alpha);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
     }
 }
